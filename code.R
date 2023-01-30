a70e74234fae434f5e32c583ce7bb483c8f0b393
Dylan Luster <dgluster@txwes.edu>
2023-01-29 23:17:57 -0600

    messing with stuff

--- code.R
+++ code.R
@@ -1,2 +1,9 @@
 library(tidyverse)
-data("iris3")
+data("iris")
+
+sepL <- iris %>% select("Species", "Sepal.Length")
+#Average sepal length for Setosa
+setSepLAvg <-  mean(sepalL$Sepal.Length[1:50])
+setSE <- sd(sepalL$Sepal.Length[1:50])/sqrt(50)
+#95% confidence interval
+ci <- c(qnorm(0.025, setSepLAvg, setSE), qnorm(0.975, setSepLAvg, setSE))
